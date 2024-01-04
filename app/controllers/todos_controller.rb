68da037cf89b4cd100f4ce2e9eb8dab85f149483
khamiruf <khamiruf@khamirufs-MacBook-Air.local>
2024-01-04 15:39:07 +0800

    feat: added CRD functionality

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,21 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(description: params[:todo][:description])
+    if @todo.valid?
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to root_path
+  end
+end
